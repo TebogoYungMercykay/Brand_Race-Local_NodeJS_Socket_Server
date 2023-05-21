c076281cfc806caf6f1e575b35b6c481e5254813
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-05-21 10:58:44 +0200

    Bug Fixes, And Choosing PORT Implementation

--- NodeJS_MultiUser_Server/server.js
+++ NodeJS_MultiUser_Server/server.js
@@ -4,12 +4,13 @@ const http = require("http");
 const axios = require('axios');
 const jquery = require('jquery');
 const socketIo = require("socket.io");
+const readline = require('readline');
 // const createAdapter = require("@socket.io/redis-adapter").createAdapter;
 // const redis = require("redis");
 // const { createClient } = redis;
 require('dotenv').config({ path: 'set_env.env' });
 
-const { UsersAndRooms, JoinLoop, getCurrentUser, LeaveGame, getUsersGameID, UserExists } = require("./Client_Side/js/users");
+const { UsersAndRooms, JoinLoop, getCurrentUser, LeaveGame, getUsersGameID } = require("./Client_Side/js/users");
 const expressApp = express();
 expressApp.use(express.urlencoded({ extended: true }));
 const nodeServer = http.createServer(expressApp);
@@ -22,106 +23,91 @@ expressApp.use(express.static(path.join(__dirname, 'Client_Side')));
 //     subClient = pubClient.duplicate();
 //     io.adapter(createAdapter(pubClient, subClient));
 // })();
+// Make the request using axios
+
+// axios.post(process.env.WURL, {"limit":"5"}, {
+//     auth: {
+//         username: process.env.WUSERNAME,
+//         password: process.env.WPASSWORD
+//     }
+// })
+// .then(response => {
+//     // Handle the response data
+//     console.log(response.data);
+//     // res.send(response.data);
+// }).catch(error => {
+//     // Handle the error
+//     console.log(error);
+//     // res.status(500).send('Error occurred while sending the request.');
+// });
 
 // When A Client Connects
 mySocket.on('connection', socket => {
     // Define a route to handle the request
-    expressApp.get('/send-request', (req, res) => {
-        console.log("Express Server Started...");
-        // Define the request payload or parameters
-        const payload = {
-            limit: 5
-        };
-        // Make the request using axios
-        axios.post(process.env.WURL, payload, {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            auth: {
-                username: process.env.WUSERNAME,
-                password: process.env.WPASSWORD
-            }
-        })
-        .then(response => {
-            // Handle the response data
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(error => {
-            // Handle the error
-            console.log(error);
-            res.status(500).send('Error occurred while sending the request.');
-        });
-    });
-    // DONE
     socket.on('EnterRace', ({ username, gameId }) => {
-        const usersInGame = getUsersGameID(gameId);
+        var usersInGame = getUsersGameID(gameId);
         console.log('Connection: ', username, gameId);
         if (usersInGame.length < 2 && !getCurrentUser(username)) {
             // Join User to server
             const addUser = JoinLoop(socket.id, username, gameId);
-            socket.join(addUser);
-            UsersAndRooms(getUsersGameID(gameId), gameId);
-
+            socket.join(addUser.gameId);
+            UsersAndRooms("Connection", getUsersGameID(gameId), gameId);
+            usersInGame = getUsersGameID(addUser.gameId);
+            console.log("Users in the Game: " + usersInGame.length, addUser.gameId);
             // Broadcast when a user connects
-            socket.broadcast.to(addUser.gameId).emit("message", `${addUser.username} has joined the chat`);
-            mySocket.emit('message', 'A user has connected to the server');
+            console.log(`Broadcasting message to ${addUser.gameId} room`);
+            socket.broadcast.to(addUser.gameId).emit('message', `${addUser.username} has joined the game`);
+            socket.emit('message', `${addUser.username} has connected to the server`);
             // Send users and gameId info
+            console.log(`Sending InTheGame event to ${addUser.gameId} room`);
             mySocket.to(addUser.gameId).emit("InTheGame", { gameId: addUser.gameId, users: usersInGame });
+            // Starting the game LOOP:
+            if (usersInGame.length == 2) {
+                console.log(`Sending StartGame event to ${addUser.gameId} room`);
+                mySocket.to(addUser.gameId).emit("StartGame", { gameId: addUser.gameId, users: usersInGame });
+            }
         } else {
             socket.emit('userAlreadyExists', username);
+            console.log('User already exists with Username: ' + username + ' on server!');
             socket.disconnect();
         }
     });
 
     // When a Client disconnects
     socket.on('disconnect', () => {
-        console.log('Client disconnect: ',socket.id);
+        console.log('Client disconnect: ', socket.id);
         const leaveRoom = LeaveGame(socket.id);
         if (leaveRoom) {
-            console.log('Disconnect: ', leaveRoom.gameId,leaveRoom.username);
-            UsersAndRooms(getUsersGameID(leaveRoom.gameId), leaveRoom.gameId);
+            console.log('Disconnect: ', leaveRoom.gameId, leaveRoom.username);
+            UsersAndRooms("Disconnect", getUsersGameID(leaveRoom.gameId), leaveRoom.gameId);
             mySocket.to(leaveRoom.gameId).emit('message', `${leaveRoom.username} has Left the Game`);
             mySocket.to(leaveRoom.gameId).emit("GamingUsers", { gameId: leaveRoom.gameId, users: getUsersGameID(leaveRoom.gameId) });
         }
         mySocket.emit('message', 'A user has disconnected from the server');
     });
 });
-// const axios = require('axios');
-expressApp.get('/send-request', (req, res) => {
-    const options = {
-        url: process.env.WURL,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        auth: {
-            username: process.env.WUSERNAME,
-            password: process.env.WPASSWORD
-        },
-        data: {limit:"5"}
-    };
-
-    axios(options).then(response => {
-        const body = response.data;
-        if (body.status !== 'success') {
-            console.log('Request Failed: ' + body.data.data);
-            res.writeHead(200, { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' });
-            res.write(JSON.stringify(body));
-            res.end();
-            return;
-        } else {
-            console.log(body);
-            res.writeHead(200, { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' });
-            res.write(JSON.stringify(r));
-            res.end();
-        }
-    })
-    .catch(error => {
-        console.log(error);
-        res.status(500).send('Error occurred while sending request');
-    });
+// * Making Sure a User Enters A PORT within the RANGE 1024 - 49141, NO RESERVED PORTS ALLOWED.
+// 1433: Microsoft SQL Server.
+// 3306: MySQL Database Server.
+// 5432: PostgreSQL Database Server.
+// 8080: HTTP alternative port commonly used for web servers.
+const readLineObject = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-var PORT = process.env.PORT || 3000;
-nodeServer.listen(PORT, () => { console.log(`Our Multi-User Server is Running on Port: ${PORT}}`); });
+function EnterDifferentPort() {
+    readLineObject.question('Enter a PORT Number for the Server within RANGE [1024:49141]: ', (portNumber) => {
+        let PORT = parseInt(portNumber);
+        // * Checking if the User Chose a Reserved Port Number:
+        if (PORT < 1024 || PORT > 49151 || PORT === 1433 || PORT === 3306 || PORT === 5432 || PORT === 8080) {
+            console.log('Invalid PORT. Please choose a different PORT: ');
+            //  * Enforcing a Valid PORT using Recursion!!.
+            EnterDifferentPort();
+        } else {
+            nodeServer.listen(PORT, () => { console.log(`Our Multi-User Server is Running on Port: ${PORT}}`); });
+            readLineObject.close();
+        }
+    });
+}
+EnterDifferentPort();
