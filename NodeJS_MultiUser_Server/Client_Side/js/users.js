dec93e5b5a8b48ad32b25fee7897a183af6cc1eb
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-05-23 14:37:27 +0200

    UPDATE: Added more Methods for extracting User info

--- NodeJS_MultiUser_Server/Client_Side/js/users.js
+++ NodeJS_MultiUser_Server/Client_Side/js/users.js
@@ -1,10 +1,30 @@
 const GameUsers = [];
-
 // Join currentUser to Loop
 function JoinLoop(id, username, gameId) {
     const currentUser = { id, username, gameId };
     GameUsers.push(currentUser);
     return currentUser;
+}
+
+function getUniqueGameIDs(){
+    const uniqueGameIds = [...new Set(GameUsers.map(user => user.gameId))];
+    return uniqueGameIds;
+}
+
+function GamesAlone(){
+    const gameIDs = getUniqueGameIDs();
+    gameIDs.forEach(gameID => {
+        var Users = getUsersGameID(gameID);
+        console.log('\nGame ID: ', gameID);
+        var userNames = Users.map(user => user.username).join(', ');
+        console.log('Players: ', userNames);
+    });
+}
+
+function allGamePlayers() {
+    GameUsers.forEach(user => {
+        console.log(`${user.username}\t${user.gameId}\t${user.id}`);
+    });
 }
 
 // Get current currentUser
@@ -17,12 +37,27 @@ function getCurrentUser(username) {
     return GameUsers.find(currentUser => currentUser.username === username);
 }
 
+function getGameID(username) {
+    var uu = GameUsers.find(currentUser => currentUser.username === username);
+    return uu.gameId;
+}
+
+function RemoveUser(username) {
+    var user = getCurrentUser(username);
+    if (user !== undefined) {
+        LeaveGame(user.gameId);
+        return 1;
+    }
+    return -1;
+}
+
 // User leaves Loop
 function LeaveGame(id) {
     const index = GameUsers.findIndex(currentUser => currentUser.id === id);
     if (index !== -1) {
         return GameUsers.splice(index, 1)[0];
     }
+    return -1;
 }
 
 // Get gameId GameUsers
@@ -38,4 +73,4 @@ function UsersAndRooms(fun, users, gameId) {
     console.log('Users:', userNames);
 }
 
-module.exports = { UsersAndRooms, JoinLoop, getCurrentUser, LeaveGame, getUsersGameID };
+module.exports = { UsersAndRooms, JoinLoop, getCurrentUser, LeaveGame, getUsersGameID, allGamePlayers, GamesAlone, RemoveUser, getGameID };
