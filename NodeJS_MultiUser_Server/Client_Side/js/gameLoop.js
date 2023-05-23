b6978cc0e34cc4dbe2beeffc455ced54e04a3aa5
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-05-23 14:36:04 +0200

    UPDATE: Game Loop and Commands added/fixed

--- NodeJS_MultiUser_Server/Client_Side/js/gameLoop.js
+++ NodeJS_MultiUser_Server/Client_Side/js/gameLoop.js
@@ -1,4 +1,7 @@
 // Sockets and Stuff
+var numWins = 0;
+// const { Socket } = require("socket.io");
+
 // Extracting Username and Room from URL
 const { username, gameId } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 const mySocket = io();
@@ -9,6 +12,23 @@ mySocket.emit('EnterRace', { username, gameId });
 // Print Message from server
 mySocket.on('message', message => {
     console.log('Message:', message);
+});
+
+// Print Message from server
+mySocket.on('StopGameFast', ({ username, gameId }) => {
+    console.log(`Game: ${gameId} Ended After ${username} Left:`);
+    StartGameLoop(username, gameId, undefined, true);
+});
+
+// Prompt the user before leaving the game
+$('#Quit').on('click', function (event) {
+    event.preventDefault();
+    const leaveRoom = confirm('Are you sure you want to quit the game?');
+    if (leaveRoom) {
+        mySocket.emit('EndGameFast', { username, gameId });
+        window.location = '../index.html';
+    }
+    StartGameLoop(undefined, undefined, undefined, true);
 });
 
 // Handle user already exists
@@ -34,6 +54,12 @@ mySocket.on('StartGame', ({ gameId, users }) => {
     StartGameLoop(users, gameId, 5, false);
 });
 
+mySocket.on('Winner', ({winner, userScore}) => {
+    // Add_Name_Winner
+    console.log('Winner Client');
+    document.getElementById('Add_Name_Winner').textContent = `${winner}<br>With score ${userScore}`;
+});
+
 // Add users to DOM
 function handleUsersAndGameId(eventType, gameId, users) {
     console.log('This made the Call:', eventType);
@@ -42,21 +68,14 @@ function handleUsersAndGameId(eventType, gameId, users) {
     console.log('Client Connected Users:', usernames);
 }
 // * GAME LOOP STARTS HERE.
-// Prompt the user before leaving the game
-$('#Quit').on('click', function (event) {
-    event.preventDefault();
-    const leaveRoom = confirm('Are you sure you want to quit the game?');
-    if (leaveRoom) {
-        window.location = '../index.html';
-    }
-    StartGameLoop(undefined, undefined, undefined, true);
-});
 // TODO: Must Refine This Function, Which Is Basically an Implementation of The Game Loop.
 // TODO: The Intervals are Perfect, I just need to make sure they are Using Images from the API on the Wheatley server and not my Local API
 function StartGameLoop(users, gameId, limit, clearTimeouts) {
     console.log("Game Loop Started");
     document.getElementById('Add_Contestants').textContent = "CONTESTANTS ARRIVED!!";
-    if (clearTimeouts == true) {
+    if (users != undefined && gameId != undefined) {
+        clearTimeouts(users, gameId);
+    } else if (clearTimeouts == true) {
         clearTimeouts();
         return;
     }
@@ -66,9 +85,10 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
     var index = -1;
     var defaultCounter = 15;
     var defaultCounter1 = 0;
+    var Answer = undefined;
     var counter = defaultCounter;
     var intervalId = undefined;
-    var winner = username;
+    var winner = 'No Winner for this Round';
     var breakOut = false;
     var arrayTimeoutReferences = [];
     var TimeoutReferencesIntervals = new Map();
@@ -82,33 +102,53 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
     $('#Submit_Brand').on('click', function (event) {
         event.preventDefault();
         const input = $('#brandName').val();
-        if (input === 'Audi') {
-            console.log('Correct Answer Form This User!!');
+        if (input == Answer) {
+            console.log(`Correct Answer from ${username}, They get a point!!`);
             // TODO: Must emit a message to the server and Use DOM for Overwriting the Labels
             // * Using the server's response object
+            // if (intervalId !== undefined) {
+            //     clearInterval(intervalId);
+            // }
+            // if (TimeoutReferencesIntervals.size !== 0) {
+            //     const lastTimeoutId = TimeoutReferencesIntervals.get(index);
+            //     console.log(index);
+            //     if (lastTimeoutId) {
+            //         clearTimeout(lastTimeoutId);
+            //     }
+            // }
+            numWins += 1;
+            toddle('LoadingScreen');
+            document.getElementById('Add_Round_Winner').textContent = winner;
+        }
+    });
+    function clearTimeouts(username2, gameId2) {
+        if (arrayTimeoutReferences !== undefined && arrayTimeoutReferences !== null) {
+            for (var i = 0; i < arrayTimeoutReferences.length; i++) {
+                if (arrayTimeoutReferences[i] != undefined) {
+                    clearTimeout(arrayTimeoutReferences[i]);
+                }
+            }
+            arrayTimeoutReferences = []; // Reset the timeout references array
+            breakOut = true;
             if (intervalId !== undefined) {
                 clearInterval(intervalId);
             }
-            if (TimeoutReferencesIntervals.size !== 0) {
-                const lastTimeoutId = TimeoutReferencesIntervals.get(index);
-                console.log(index);
-                if (lastTimeoutId) {
-                    clearTimeout(lastTimeoutId);
+            document.getElementById('Add_Contestants').textContent = `${username2} Has Left The Game: ${gameId2}`;
+            toddle('Landing_div');
+        }
+    }
+    function clearTimeouts() {
+        if (arrayTimeoutReferences !== undefined && arrayTimeoutReferences !== null) {
+            for (var i = 0; i < arrayTimeoutReferences.length; i++) {
+                if (arrayTimeoutReferences[i] != undefined) {
+                    clearTimeout(arrayTimeoutReferences[i]);
                 }
             }
-            toddle('LoadingScreen');
-        }
-    });
-    function clearTimeouts() {
-        for (var i = 0; i < arrayTimeoutReferences.length; i++) {
-            if (arrayTimeoutReferences[i] != undefined) {
-                clearTimeout(arrayTimeoutReferences[i]);
+            arrayTimeoutReferences = []; // Reset the timeout references array
+            breakOut = true;
+            if (intervalId !== undefined) {
+                clearInterval(intervalId);
             }
-        }
-        arrayTimeoutReferences = []; // Reset the timeout references array
-        breakOut = true;
-        if (intervalId !== undefined) {
-            clearInterval(intervalId);
         }
     }
     function StartGameLoopNow(limit) {
@@ -118,7 +158,9 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
         var json = JSON.stringify(jsonObject);
         // console.log(json);
         $.ajax({
-            url: "https://wheatley.cs.up.ac.za/u20748052/hwa_api.php",
+            // url: "https://wheatley.cs.up.ac.za/u20748052/hwa_api.php",
+            // url: "http://localhost/API_MYSQL_DATABSE/php/api.php/GetRandomBrands",
+            url: 'https://u20748052:@Tebogo1357963729@wheatley.cs.up.ac.za/u20748052/hwa_api.php',
             method: "POST",
             data: json,
             success: function(response) {
@@ -133,6 +175,7 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
                 toddle('DisplayStuff');
                 if(breakOut == false){
                     timeoutReference = setTimeout(function() {
+                        winner = 'No Winner for this Round';
                         console.log("Displaying Image ==> ", 0);
                         index = 0;
                         counter = defaultCounter;
@@ -150,6 +193,7 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
                         }, 1000);
                         toddle('InTheGame');
                         document.getElementById('imagelogo').src = images[0].image_url;
+                        Answer = images[0].image_name;
                         TimeoutReferencesIntervals.set(0, timeoutReference);
                     }, 4500);
                     arrayTimeoutReferences.push(timeoutReference);
@@ -167,6 +211,7 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
                                 }, time-5000);
                                 arrayTimeoutReferences.push(timeoutReference);
                                 timeoutReference = setTimeout(function() {
+                                    winner = 'No Winner for this Round';
                                     console.log("Displaying Image ==> ", j);
                                     index = j;
                                     counter = defaultCounter;
@@ -184,6 +229,7 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
                                     }, 1000);
                                     toddle('InTheGame');
                                     document.getElementById('imagelogo').src = images[j].image_url;
+                                    Answer = images[j].image_name;
                                     TimeoutReferencesIntervals.set(index, timeoutReference);
                                 }, time);
                                 arrayTimeoutReferences.push(timeoutReference);
@@ -198,6 +244,7 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
                                 }, time2-5000);
                                 arrayTimeoutReferences.push(timeoutReference);
                                 timeoutReference = setTimeout(function() {
+                                    winner = 'No Winner for this Round';
                                     console.log("Displaying Image ==> ", j);
                                     index = j;
                                     counter = defaultCounter;
@@ -215,6 +262,7 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
                                     }, 1000);
                                     toddle('InTheGame');
                                     document.getElementById('imagelogo').src = images[j].image_url;
+                                    Answer = images[j].image_name;
                                     TimeoutReferencesIntervals.set(index, timeoutReference);
                                 }, time2);
                                 arrayTimeoutReferences.push(timeoutReference);
@@ -225,8 +273,9 @@ function StartGameLoop(users, gameId, limit, clearTimeouts) {
                 if(breakOut == false){
                     timeoutReference = setTimeout(function(){
                         console.log("Done Now!!!");
+                        mySocket.emit('Score', {numWins, username, gameId})
                         toddle('gameOver');
-                        document.getElementById('Add_Name_Winner').textContent = winner;
+                        document.getElementById('Add_Name_Winner').textContent = "To Be Announced!";
                     },105000);
                     arrayTimeoutReferences.push(timeoutReference);
                 }
